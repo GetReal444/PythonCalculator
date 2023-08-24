4ee79e4812d3f42f17a8a722d1547754da0afbc6
GetReal444 <141826207+GetReal444@users.noreply.github.com>
2023-08-24 20:02:11 +1000

    1.0.3 - commented code

--- calculator.py
+++ calculator.py
@@ -1,23 +1,28 @@
-ops = ["+", "-", '*', "/", "^"]
+ops = ["+", "-", '*', "/", "^"]  # allowed operators
+# operator functionality
 operators = {"+": lambda x, y: x + y,
              "-": lambda x, y: x - y,
              "*": lambda x, y: x * y,
              "/": lambda x, y: x / y,
              "^": lambda x, y: x ** y}
 
-operator = input("Enter an operator " + f"{ops}: ")
+operator = input("Enter an operator " + f"{ops}: ")  # operator input
 
+# operator validity check
 if operator not in operators:
     print(f"{operator} not a valid operator")
 
+# number 1, number 2
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 
+# returns the number values
 def get_inputs():
     return num1, num2
 
 
+# calculation
 get_inputs()
 results = operators[operator](num1, num2)
 print(round(results, 3))
